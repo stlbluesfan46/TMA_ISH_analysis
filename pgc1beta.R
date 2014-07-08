02861b4b59e3cb25f28de061fe46fe08eafada96
stlbluesfan46 <stlbluesfan46@user.noreply.github.com>
2014-07-08 17:39:17 -0500

    Removed old crappy jpeg plots.
    Cleaned up and added annotations.

--- pgc1beta.R
+++ pgc1beta.R
@@ -1,58 +1,27 @@
-#PGC1-beta plots
+#PGC1beta plots
 
-jpeg("PGC1betaAllFoldChange.jpg")
-plot(probe_fc$Stage, probe_fc$fc, main = "PGC1beta compared to Normal")
-dev.off()
+#packages needed to make pretty plots
+library(ggplot2, plyr)
 
-jpeg("PGC1betaAllFoldChangeBar.jpg")
-barplot(stage_average_fc)
-dev.off()
-
-jpeg("RawData.jpg")
-plot(probe$Stage, probe$mRNA, main = "Raw Data, no background subtraction")
-dev.off()
-
-threshold_probe <- tapply(probe$Threshold, list(probe$Stage), mean, na.rm = T)
+#raw data
 ourData <- subset(probe, probe$TMA != "TMA3")
 wangData <- subset(probe, probe$TMA == "TMA3")
 
-jpeg("NoTMA3Raw.jpg")
-plot(ourData$Stage, ourData$mRNA, main = "Raw Data, no TMA3")
-dev.off()
+#background subtracted and averaged to sample
+ourDataNBG <- subset(probe_PA, probe_PA$TMA != "TMA3")
+wangDataNBG <- subset(probe_PA, probe_PA$TMA == "TMA3")
 
-jpeg("TMA3Raw.jpg")
-plot(wangData$Stage, wangData$mRNA, main = "Wang TMA only")
-dev.off()
-
-jpeg("ByTMA.jpg")
-plot(probe_fc$TMA, probe_fc$fc, main = "By TMA")
-dev.off()
-
-jpeg("BackgroundSubAll.jpg")
-plot(probe_nobkgd$Stage, probe_nobkgd$pmnp, main = "Background Subtracted All")
-dev.off()
-
-ourDataNBG <- subset(probe_nobkgd, probe_nobkgd$TMA != "TMA3")
-wangDataNBG <- subset(probe_nobkgd, probe_nobkgd$TMA == "TMA3")
-
-jpeg("NoTMA3NBKG.jpg")
-plot(ourDataNBG$Stage, ourDataNBG$pmnp, main = "Background Subtracted, no TMA3")
-dev.off()
-
-jpeg("TMA3NBKG.jpg")
-plot(wangDataNBG$Stage, wangDataNBG$pmnp, main = "Background Stubtacted, Wang TMA only")
-dev.off()
-
+#some threshold info
 threshold_TMA <- tapply(probe$Threshold, list(probe$TMA), mean, na.rm = T)
 threshold_ourData <-tapply(ourData$Threshold, list(ourData$Stage), mean, na.rm = T)
 threshold_wangData <-tapply(wangData$Threshold, list(wangData$Stage), mean, na.rm = T)
 
 #Stats on our data TMA's 1,2, 4,5,6  NO 3
-ourDataNBG_aov <- aov(pmnp~Stage, ourDataNBG)
+ourDataNBG_aov <- aov(Average~Stage, ourDataNBG)
 summary(ourDataNBG_aov)
 TukeyHSD(ourDataNBG_aov)
 
-
+#Subsetting our data wihtout TMA into separte stages to run t tests on each
 ourDataNBG_Normal <- subset(ourDataNBG, ourDataNBG$Stage == "Normal")
 ourDataNBG_Stage1 <- subset(ourDataNBG, ourDataNBG$Stage == "Stage 1")
 ourDataNBG_Stage2 <- subset(ourDataNBG, ourDataNBG$Stage == "Stage 2")
@@ -63,37 +32,47 @@ ourDataNBG_Stage4Met <- subset(ourDataNBG, ourDataNBG$Stage == "Stage 4 Met")
 ourDataNBG_TA <- subset(ourDataNBG, ourDataNBG$Stage == "Tubular Adenoma")
 
 
+#the t tests
+ourDataNBG_ttest <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage1$Average)
+ourDataNBG_ttest2 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage2$Average)
+ourDataNBG_ttest3 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage3$Average)
+ourDataNBG_ttest4 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage4$Average)
+ourDataNBG_ttest5 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage3Met$Average)
+ourDataNBG_ttest6 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_Stage4Met$Average)
+ourDataNBG_ttest7 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_TA$Average)
 
-ourDataNBG_ttest <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage1$pmnp)
-ourDataNBG_ttest2 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage2$pmnp)
-ourDataNBG_ttest3 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage3$pmnp)
-ourDataNBG_ttest4 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage4$pmnp)
-ourDataNBG_ttest5 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage3Met$pmnp)
-ourDataNBG_ttest6 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_Stage4Met$pmnp)
-ourDataNBG_ttest7 <- t.test(ourDataNBG_Normal$pmnp, ourDataNBG_TA$pmnp)
 
-ourDataNBG_stat <- ddply(ourDataNBG, .(Stage), summarise, mean = mean(pmnp), sd = sd(pmnp))
+#data for error bars for plot 1
+ourDataNBG_stat <- ddply(ourDataNBG, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
 
-ggplot(ourDataNBG, aes(x = ourDataNBG$Stage, y = ourDataNBG$pmnp)) +
+#plot 1 - scatter plot with error bars, all data except TMA 3
+ggplot(ourDataNBG, aes(x = Stage, y = Average)) +
   geom_point(aes(color = factor(ourDataNBG$TMA)), size = 5) + 
   geom_point(data = ourDataNBG_stat, aes(x = Stage, y = mean), colour = 'black', size = 5) +
-  geom_errorbar(data = ourDataNBG_stat, aes(x = Stage, y = pmnp, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
+  geom_errorbar(data = ourDataNBG_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
   labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
-
 
 ggsave(
   "PGC1beta plot 1.png",
   dpi = 500
 )
 
+#subsetting normals, stage 4, stage 4 mets, no tma 3 samples
 ourDataNPM <- subset(ourDataNBG, ourDataNBG$Stage == "Normal" | ourDataNBG$Stage == "Stage 4" | ourDataNBG$Stage == "Stage 4 Met")
 
-ourDataNPM_stat <- ddply(ourDataNPM, .(Stage), summarise, mean = mean(pmnp), sd = sd(pmnp))
+#stats for plot 2
+ourDataNPM_aov <- aov(Average~Stage, ourDataNPM)
+summary(ourDataNPM_aov)
+TukeyHSD(ourDataNPM_aov)
 
-ggplot(ourDataNPM, aes(x = Stage, y = pmnp)) +
-  geom_point(size = 3) + 
-  geom_point(data = ourDataNPM_stat, aes(x = Stage, y = mean), colour = 'black', size = 5) +
-  geom_errorbar(data = ourDataNPM_stat, aes(x = Stage, y = pmnp, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
+#for error bars for plot 2
+ourDataNPM_stat <- ddply(ourDataNPM, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
+
+#plot 2 - Normals vs Stage 4 Primrary vs Stage 4 Met, no TMA 3
+ggplot(ourDataNPM, aes(x = Stage, y = Average)) +
+  geom_point(size = 5) + 
+  geom_point(data = ourDataNPM_stat, aes(x = Stage, y = mean), colour = 'red', size = 6) +
+  geom_errorbar(data = ourDataNPM_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
   labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
 
 ggsave(
@@ -103,12 +82,14 @@ ggsave(
   dpi = 500
 )
 
-probe_nobkgd_stat <- ddply(probe_nobkgd, .(Stage), summarise, mean = mean(pmnp), sd = sd(pmnp))
+#error bar info for plot 3
+probe_PA_stat <- ddply(probe_PA, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
 
-ggplot(probe_nobkgd, aes(x = Stage, y = pmnp)) +
+#plot of all data, including TMA 3
+ggplot(probe_PA, aes(x = Stage, y = Average)) +
   geom_point(aes(color = factor(TMA)), size = 5, position = "dodge") + 
-  geom_point(data = probe_nobkgd_stat, aes(x = Stage, y = mean), colour = 'black', size = 6) +
-  geom_errorbar(data = probe_nobkgd_stat, aes(x = Stage, y = pmnp, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
+  geom_point(data = probe_PA_stat, aes(x = Stage, y = mean), colour = 'black', size = 6) +
+  geom_errorbar(data = probe_PA_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
   labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
 
 ggsave(
