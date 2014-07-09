d32bdbd839c856fadf3cbf8ee390719f73089a50
stlbluesfan46 <stlbluesfan46@user.noreply.github.com>
2014-07-09 15:57:16 -0500

    updated graphs
    changed so using sample average, not individual spots

--- pgc1beta.R
+++ pgc1beta.R
@@ -1,7 +1,8 @@
 #PGC1beta plots
-
+#run the analysis first with pgc1beta.csv
 #packages needed to make pretty plots
-library(ggplot2, plyr)
+library(ggplot2)
+library(plyr)
 
 #raw data
 ourData <- subset(probe, probe$TMA != "TMA3")
@@ -43,14 +44,14 @@ ourDataNBG_ttest7 <- t.test(ourDataNBG_Normal$Average, ourDataNBG_TA$Average)
 
 
 #data for error bars for plot 1
-ourDataNBG_stat <- ddply(ourDataNBG, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
+ourDataNBG_stat <- ddply(ourDataNBG, .(Stage), summarise, mean = mean(Average, na.rm = T), sd = sd(Average, na.rm = T))
 
 #plot 1 - scatter plot with error bars, all data except TMA 3
 ggplot(ourDataNBG, aes(x = Stage, y = Average)) +
-  geom_point(aes(color = factor(ourDataNBG$TMA)), size = 5) + 
+  geom_point(aes(color = factor(ourDataNBG$TMA)), size = 5, na.rm = T) + 
   geom_point(data = ourDataNBG_stat, aes(x = Stage, y = mean), colour = 'black', size = 5) +
   geom_errorbar(data = ourDataNBG_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
-  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
+  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = expression(paste("mRNA / Area (\u03BCm"^"2",")")), colour = "")
 
 ggsave(
   "PGC1beta plot 1.png",
@@ -66,33 +67,47 @@ summary(ourDataNPM_aov)
 TukeyHSD(ourDataNPM_aov)
 
 #for error bars for plot 2
-ourDataNPM_stat <- ddply(ourDataNPM, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
+ourDataNPM_stat <- ddply(ourDataNPM, .(Stage), summarise, mean = mean(Average, na.rm = T), sd = sd(Average, na.rm = T))
 
 #plot 2 - Normals vs Stage 4 Primrary vs Stage 4 Met, no TMA 3
 ggplot(ourDataNPM, aes(x = Stage, y = Average)) +
-  geom_point(size = 5) + 
-  geom_point(data = ourDataNPM_stat, aes(x = Stage, y = mean), colour = 'red', size = 6) +
-  geom_errorbar(data = ourDataNPM_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
-  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
+  geom_point(size = 4, position = position_jitter(width = 0.0375)) + 
+  scale_x_discrete(labels=c("Normal", "Primary", "Metastasis")) +
+  geom_point(data = ourDataNPM_stat, aes(x = Stage, y = mean), colour = 'black', size = 25, shape = 95) +
+  geom_errorbar(data = ourDataNPM_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.3) +
+  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = expression(paste("mRNA / Area (\u03BCm"^"2",")")), colour = "") +
+  theme_bw()
 
 ggsave(
   "PGC1beta plot 2.png",
-  width = 5.0,
-  height = 5.0,
   dpi = 500
 )
 
-#error bar info for plot 3
-probe_PA_stat <- ddply(probe_PA, .(Stage), summarise, mean = mean(Average), sd = sd(Average))
-
-#plot of all data, including TMA 3
-ggplot(probe_PA, aes(x = Stage, y = Average)) +
-  geom_point(aes(color = factor(TMA)), size = 5, position = "dodge") + 
-  geom_point(data = probe_PA_stat, aes(x = Stage, y = mean), colour = 'black', size = 6) +
-  geom_errorbar(data = probe_PA_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
-  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = "mRNA / Area(pixels)", colour = "")
+#plot 3 - Bar plot of plot 2
+ggplot(ourDataNPM_stat, aes(x = Stage, y = mean)) +
+  geom_bar(aes(fill=Stage), stat = "identity") + 
+  scale_x_discrete(labels=c("Normal", "Primary", "Metastasis")) +
+  geom_errorbar(data = ourDataNPM_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
+  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = expression(paste("mRNA / Area (\u03BCm"^"2",")")), colour = "") +
+  scale_fill_grey(start = 0.9, end = 0) +
+  theme_bw()
 
 ggsave(
   "PGC1beta plot 3.png",
+  dpi = 500
+)
+
+#error bar info for plot 4
+probe_PA_stat <- ddply(probe_PA, .(Stage), summarise, mean = mean(Average, na.rm = T), sd = sd(Average, na.rm = T))
+
+#plot of all data, including TMA 3
+ggplot(probe_PA, aes(x = Stage, y = Average)) +
+  geom_point(aes(color = factor(TMA)), size = 5, position = position_jitter(width = 0.1)) + 
+  geom_point(data = probe_PA_stat, aes(x = Stage, y = mean), colour = 'black', size = 6) +
+  geom_errorbar(data = probe_PA_stat, aes(x = Stage, y = Average, ymin = mean - sd, ymax = mean + sd), width = 0.4) +
+  labs(title = "PGC1beta in Human Colon Cancer Samples", x = "", y = expression(paste("mRNA / Area (\u03BCm"^"2",")")), colour = "")
+
+ggsave(
+  "PGC1beta plot 4.png",
   dpi = 500
 )
